c9c8564e25521b94ecd2c141baa039b80ee67678
Maneesha Kalubowila <73266185+KalubowilaDMR@users.noreply.github.com>
2022-08-31 20:11:46 +0530

    final upload

--- backend/app/Http/Controllers/API/StudentController.php
+++ backend/app/Http/Controllers/API/StudentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
 
 class StudentController extends Controller
 {
@@ -19,17 +21,110 @@ class StudentController extends Controller
 
     public function store(Request $request){
 
-        $student = new Student;
-        $student->name = $request->input('name');
-        $student->course = $request->input('course');
-        $student->email = $request->input('email');
-        $student->phone = $request->input('phone');
-        $student->save();
-
-        return response()->json([
-            'status' => 200,
-            'message' => 'Student Added Successfully.',
+        $validator = Validator::make($request ->all(),[
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|min:10|max:10',
         ]);
+        
+        if($validator->fails())
+        {
+            return response()->json([
+                'validate_err' => $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $student = new Student;
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+            $student->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Added Successfully.',
+            ]);
+        }        
 
     }
+
+    public function edit($id)
+    {
+    
+        $student = Student::find($id);
+        if($student){
+            return response()->json([
+                'status' => 200,
+                'student' => $student,
+            ]);
+        }
+        else{
+            return response()->json([
+                'status' => 404,
+                'message' => 'Student Record ID NOT Found.',
+            ]);
+        }
+        
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $validator = Validator::make($request ->all(),[
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|min:10|max:10',
+        ]);
+        
+        if($validator->fails())
+        {
+            return response()->json([
+                'validate_err' => $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $student = Student::find($id);
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+            $student->update();
+
+            if($student)
+            {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Student record Updated Successfully.',
+                    'student' => $student,
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Student record Updated Error Occured.',
+                ]);
+            }
+            
+        }
+        
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        $student->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student record deleted successfully.',
+            'student' => $student,
+        ]);
+    }
+
 }
