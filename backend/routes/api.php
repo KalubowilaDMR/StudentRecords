ed2a5d73a9060a2924cb89483234a1d326badd76
Maneesha Kalubowila <73266185+KalubowilaDMR@users.noreply.github.com>
2022-08-31 20:15:53 +0530

    Add files via upload

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/students', [StudentController::class, 'index']);
 Route::post('/add-student', [StudentController::class, 'store']);
+Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+Route::put('/update-student/{id}', [StudentController::class, 'update']);
+Route::delete('/delete-student/{id}', [StudentController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
